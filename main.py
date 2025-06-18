54382b41023a0c5630281dfde3057a142fd8848c
syma sultana <symasultana2002@gmail.com>
2025-06-18 22:50:48 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,82 @@
+print("syma sultana")
+print("monika")
+print("rinty")
+
+name = "mala"
+age= 23
+age2= age
+height = 67.89
+
+print(name)
+print(type(name))
+print(type(height))
+print(age2)
+print(height)
+
+
+
+
+old = True
+a = None
+print(type(old))
+print(type(a))
+
+a= 23
+b=34
+sum = a+b
+print(sum)
+
+# arithmetic operators
+
+a=34
+b=12
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)#reminder
+print(a**b)#a^b
+
+
+
+# relational operators
+#it always return the true false output
+
+a=2
+b=4
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a<b)
+print(a>b)
+
+
+# assignment operator
+num = 12
+num%= 6
+print("num:", num)# show the reminder
+
+# logical operator
+
+a=45
+b=56
+print(not False)#output true
+print(not (a>b))#output true
+val1= True
+val2= False
+
+print("the output with AND:",val1 and val2)
+print("the output with OR:",val1 or val2)
+
+#input example
+name=input("enter your name:")
+age=input("enter your age:")
+profession=input("enter your profession:")
+print(name,age, profession)
+
+val=int(input("enter your value:"))
+print(type(val))
+
+
+
