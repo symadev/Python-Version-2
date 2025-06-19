60d45295d4e8d467fb37ded3643a2bcff2da6423
syma sultana <symasultana2002@gmail.com>
2025-06-20 01:08:20 +0600

    added the dict

---
+++ lecture-4.py
@@ -0,0 +1,14 @@
+dict={
+    "name":"rutu",
+     "age":23,
+    "height": 56.6,
+    list:[34,56,7,78,90],
+
+
+}
+print(dict)
+print(type(dict))
+print(dict["name"])
+print(dict["height"])
+print(dict[list])
+print(list(dict))
