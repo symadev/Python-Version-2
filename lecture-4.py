7a356579a897bb58852d52f7546a7b78e57a6a20
syma sultana <symasultana2002@gmail.com>
2025-06-20 01:33:43 +0600

    added the sets example with union and intersection

--- lecture-4.py
+++ lecture-4.py
@@ -25,3 +25,11 @@ print(dict)#just value is replaced
 collection= { 23,3,4,45,"masha","ritika","monika"}
 print(collection)
 print(type(collection))
+collection.add(49)
+collection.remove(49)
+collection.pop()#first element  delete  in this pop operation
+print(collection)
+collection2= {23,45,6789,90,7,2}
+print(collection2.union(collection))#print with added the collection2
+print(collection2.intersection(collection))#show the common elememnt
+# set ignore the duplicate items
