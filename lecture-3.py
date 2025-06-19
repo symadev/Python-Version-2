41a6226f60dbe2ac004f72cfe192817ff0b6b47b
syma sultana <symasultana2002@gmail.com>
2025-06-20 00:41:48 +0600

    added the lecture 3

---
+++ lecture-3.py
@@ -0,0 +1,35 @@
+marks = [12,23,34,"kurup",23.34]
+print(marks)
+print(type(marks))
+print(marks[2:])
+print(marks[0:4])
+
+list=[56, 45]
+
+print(list.append(3))
+print(list.sort())
+print(list.sort(reverse=True))
+print(list.reverse())
+print(list.insert(2,1))
+print(list.remove(56))
+print(list.pop(2))#remove element from index
+print(list)
+
+
+#touples
+tup =(23,3,223,3,34,)
+print(tup)
+print(type(tup))
+print(tup.index(3))
+print(tup.count(3))
+
+
+movies=[]
+movie1=input("my first movie is :")
+movies.append(movie1)
+movie2=input("my second movie is :")
+movies.append(movie2)
+movie3=input("my third movie is :")
+movies.append(movie3)
+print(movies)
+
