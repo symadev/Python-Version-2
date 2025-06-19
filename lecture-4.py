e8b70e4eb18da47e913ed1e521f6dcb545b55fea
syma sultana <symasultana2002@gmail.com>
2025-06-20 01:16:35 +0600

    added teh updation

--- lecture-4.py
+++ lecture-4.py
@@ -12,3 +12,9 @@ print(dict["name"])
 print(dict["height"])
 print(dict[list])
 print(list(dict))
+new_dict = {
+    "name":"hena",
+    "age":34
+}
+dict.update(new_dict)
+print(dict)#just value is replaced
