e8f357910cd02a7156ae35cd6e2534f61610a887
syma sultana <symasultana2002@gmail.com>
2025-06-21 00:22:40 +0600

    added loops example

---
+++ lecture-5.py
@@ -0,0 +1,18 @@
+#loop
+i= 1
+while i<=10:
+    print("apna college")
+    i+=1
+
+i=2
+while i<=16:
+    print("monika")
+    i+=2
+
+i=5
+while i > 6:
+    print(i)
+    i-=1
+print("loop ended")
+
+
